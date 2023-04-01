377c78d9a47d62dc912253ec9c24998fc72c1436
26tanishabanik <26tanishabanik@gmail.com>
2023-04-02 03:40:40 +0530

    formatted models.go
    Signed-off-by: 26tanishabanik <26tanishabanik@gmail.com>

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,12 +1,12 @@
 package models
 
 type Response struct {
-	Entity        string      `json:"entity"`
-	IptableOutput interface{} `json:"iptableOutput"`
+	Entity        string         `json:"entity"`
+	IptableOutput interface{}    `json:"iptableOutput"`
 }
 
 type KubernetesDefaultResponse struct {
-    PodName       string      `json:"podName"`
+    PodName       string         `json:"podName"`
 	PodList		  []string       `json:"podNames"`
-    IptableOutput interface{} `json:"iptableOutput"`
+    IptableOutput interface{}    `json:"iptableOutput"`
 }
