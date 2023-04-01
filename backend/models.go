fd9c77402a5d46f553b99ff855ff68ae18cf2512
26tanishabanik <26tanishabanik@gmail.com>
2023-04-02 01:40:13 +0530

    updated main and removed pod list route
    Signed-off-by: 26tanishabanik <26tanishabanik@gmail.com>

---
+++ backend/models.go
@@ -0,0 +1,17 @@
+package main
+
+type Response struct {
+	Entity        string      `json:"entity"`
+	IptableOutput interface{} `json:"iptableOutput"`
+}
+
+type KubernetesDefaultResponse struct {
+    PodName       string      `json:"podName"`
+	PodList		  []string       `json:"podNames"`
+    IptableOutput interface{} `json:"iptableOutput"`
+}
+
+type PageData struct {
+	Title string
+	Body  string
+}
