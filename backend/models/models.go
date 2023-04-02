8647588c26b541e3fb26ff05bebd00a52dcfcb62
26tanishabanik <26tanishabanik@gmail.com>
2023-04-02 11:34:06 +0530

    fixed iptable json name
    Signed-off-by: 26tanishabanik <26tanishabanik@gmail.com>

--- backend/models/models.go
+++ backend/models/models.go
@@ -2,11 +2,11 @@ package models
 
 type Response struct {
 	Entity        string      `json:"entity"`
-	IptableOutput interface{} `json:"iptableOutput"`
+	IptableOutput interface{} `json:"iptablesOutput"`
 }
 
 type KubernetesDefaultResponse struct {
 	CurrentPodName string      `json:"currentPodName"`
 	PodList        []string    `json:"podNames"`
-	IptableOutput  interface{} `json:"iptableOutput"`
+	IptableOutput  interface{} `json:"iptablesOutput"`
 }
